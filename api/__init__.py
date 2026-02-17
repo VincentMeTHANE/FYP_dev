8fe6d8b3234cdf6eb9c82631aeef1ce7f56d0d5b
VincentMeTHANE <lunatic.vincents@gmail.com>
2026-02-17 20:13:30 +0800

    fix: register the router

--- api/__init__.py
+++ api/__init__.py
@@ -9,6 +9,7 @@ from api.api_write_report_serp import router as write_report_serp
 from api.api_write_report_ask_questions import router as write_report_ask_questions
 from api.api_write_report_plan import router as write_report_plan
 from api.api_write_report_search import router as write_report_search
+from api.api_write_report_search_summary import router as write_report_search_summary
 from api.api_write_report_final import router as write_report_final
 
 
@@ -16,6 +17,7 @@ def setup_routers(app: FastAPI):
     app.include_router(base_router, prefix="")
     app.include_router(report, prefix="/report")
     app.include_router(write_report_serp, prefix="/serp")
+    app.include_router(write_report_search_summary, prefix="/summary")
     app.include_router(write_report_ask_questions, prefix="/ask_questions")
     app.include_router(write_report_plan, prefix="/plan")
     app.include_router(write_report_search, prefix="/search")
